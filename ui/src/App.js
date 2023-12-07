0ce9a83d3be6eed828e8054c107766db6fa841f9
sajmaru <sajmaru360@gmail.com>
2023-12-07 18:49:46 -0500

    test

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,33 +1,26 @@
-import './App.css';
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import SideBar from "./components/SideBar/SideBar";
 import ErrorPage from "./components/Error/Error";
 import DataFinder from "./components/Data Finder/DataFinder";
 
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const router = createBrowserRouter([
-    {
-        element: <SideBar />,
-        errorElement: <ErrorPage />,
-        children: [
-              {
-                path: "/",
-                element: <DataFinder />,
-              }
-        ]
-    }
+  {
+    element: <SideBar />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <DataFinder />,
+      },
+    ],
+  },
 ]);
 
 function App() {
-    return (
-        <GoogleOAuthProvider clientId="484642080236-5mqjaglq1i61v76vttcprlocdsl4e2bi.apps.googleusercontent.com">
-            <RouterProvider router={router} />
-        </GoogleOAuthProvider>
-    );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
